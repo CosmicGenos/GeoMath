127fe7ee9eca7943c8d05a2123482824b2b5ef19
CosmicGenos <nimesha12417@usci.ruh.ac.lk>
2026-07-13 09:41:49 +0530

    first commit

---
+++ headers/Point.h
@@ -0,0 +1,8 @@
+//
+// Created by Kavindu on 7/13/2026.
+//
+
+#ifndef GEOMATH_POINT_H
+#define GEOMATH_POINT_H
+
+#endif //GEOMATH_POINT_H
---
+++ src/main.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Kavindu on 7/13/2026.
+//
